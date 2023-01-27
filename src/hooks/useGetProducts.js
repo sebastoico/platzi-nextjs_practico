5a775bc9a60dc89e1c8471bcdf6f210db842fddd
sebastoico <sejaramillomo@gmail.com>
2023-01-26 20:36:02 -0500

    16 - Preparando la app para el deploy a producción

--- src/hooks/useGetProducts.js
+++ src/hooks/useGetProducts.js
@@ -9,7 +9,7 @@ const useGetProducts = (API) => {
       setProducts(response.data);
     };
     fetchData();
-  }, []);
+  }, [API]);
 
   return products;
 };
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,6 @@
 import '@styles/globals.css';
 import Header from '@components/Header.jsx';
+import Script from 'next/script';
 import AppContext from '@context/AppContext';
 import useInitialState from '@hooks/useInitialState';
 
@@ -7,6 +8,15 @@ export default function App({ Component, pageProps }) {
   const initialState = useInitialState();
   return (
     <AppContext.Provider value={initialState}>
+      <Script async src="https://www.googletagmanager.com/gtag/js?id=G-Z9QM4DKLT4"></Script>
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'G-Z9QM4DKLT4');
+        `}
+      </Script>
       <Header />
       <Component {...pageProps} />;
     </AppContext.Provider>
