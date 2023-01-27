bb24c79b1de7802cb96c33c2ce104a9747bd2683
sebastoico <sejaramillomo@gmail.com>
2023-01-26 20:08:11 -0500

    15 - Google Analytics con Next.js

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,10 +1,22 @@
 import { Html, Head, Main, NextScript } from 'next/document';
+import Script from 'next/script';
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head>
         <title>Yard Sale</title>
+
+        <Script async src="https://www.googletagmanager.com/gtag/js?id=G-Z9QM4DKLT4"></Script>
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-Z9QM4DKLT4');
+          `}
+        </Script>
+
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" />
         <link href="https://fonts.googleapis.com/css2?family=Quicksand:wght@400;500;700&display=swap" rel="stylesheet" />
--- src/pages/checkout.js
+++ src/pages/checkout.js
@@ -1,4 +1,5 @@
 import React, { useContext } from 'react';
+import Head from 'next/head';
 import OrderItem from '@components/OrderItem';
 import AppContext from '@context/AppContext';
 import styles from '@styles/Checkout.module.scss';
@@ -15,23 +16,28 @@ const Checkout = () => {
   };
 
   return (
-    <div className={styles.Checkout}>
-      <div className={styles['Checkout-container']}>
-        <h1 className={styles.title}>My order</h1>
-        <div className={styles['Checkout-content']}>
-          <div className={styles.order}>
-            <p>
-              <span>04.25.2021</span>
-              <span>{cart.length} articles</span>
-            </p>
-            <p>$ {sumTotal()}.00</p>
+    <>
+      <Head>
+        <title>Checkout</title>
+      </Head>
+      <div className={styles.Checkout}>
+        <div className={styles['Checkout-container']}>
+          <h1 className={styles.title}>My order</h1>
+          <div className={styles['Checkout-content']}>
+            <div className={styles.order}>
+              <p>
+                <span>04.25.2021</span>
+                <span>{cart.length} articles</span>
+              </p>
+              <p>$ {sumTotal()}.00</p>
+            </div>
+            {cart.map((product, index) => (
+              <OrderItem product={product} key={`orderItem-${product.id + index}`} indexValue={index} />
+            ))}
           </div>
-          {cart.map((product, index) => (
-            <OrderItem product={product} key={`orderItem-${product.id + index}`} indexValue={index} />
-          ))}
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
